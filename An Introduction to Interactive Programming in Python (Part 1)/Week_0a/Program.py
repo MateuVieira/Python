225884d16042966a777433b9d792088c46d3616f
MateuVieira <mateus12v@hotmail.com>
2018-12-01 19:12:32 -0200

    Create Program.py

---
+++ An Introduction to Interactive Programming in Python (Part 1)/Week_0a/Program.py
@@ -0,0 +1,226 @@
+# ------------------------------------------------------------------------------------
+# Compute the number of feet corresponding to a number of miles.
+
+###################################################
+# Miles to feet conversion formula
+# Student should enter statement on the next line.
+
+miles_to_feet = 5280
+miles = 13
+_13_miles_to_feet = miles_to_feet * miles
+print('Exercises 1:')
+print('Result exercise 1: {}'.format(_13_miles_to_feet))
+print('\n----------------------------------------------\n')
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#68640
+# ------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------
+# Compute the number of seconds in a given number of hours, minutes, and seconds.
+
+###################################################
+# Hours, minutes, and seconds to seconds conversion formula
+# Student should enter statement on the next line.
+Hours = 7
+minutes = 21
+seconds = 37
+
+result_2 = (((Hours * 60) +  minutes) * 60 ) + 60
+print('Exercises 2:')
+print('Result exercise 2: {}'.format(result_2))
+print('\n----------------------------------------------\n')
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#26497
+# ------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------
+# Compute the length of a rectangle's perimeter, given its width and height.
+
+###################################################
+# Rectangle perimeter formula
+# Student should enter statement on the next line.
+
+w = 4
+h = 7
+
+result_3 = (2*w) + (2*h)
+
+print('Exercises 3:')
+print('Result exercise 3: {}'.format(result_3))
+print('\n----------------------------------------------\n')
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#22
+# ------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------
+# Compute the area of a rectangle, given its width and height.
+
+###################################################
+# Rectangle area formula
+# Student should enter statement on the next line.
+
+w = 4
+h = 7
+
+result_4 = w*h
+
+print('Exercises 4:')
+print('Result exercise 4: {}'.format(result_4))
+print('\n----------------------------------------------\n')
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#28
+# ------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------
+# Compute the circumference of a circle, given the length of its radius.
+
+###################################################
+# Circle circumference formula
+# Student should enter statement on the next line.
+
+r = 8
+pi = 3.14
+
+result_5 = 2*pi*r
+
+print('Exercises 5:')
+print('Result exercise 5: {}'.format(result_5))
+print('\n----------------------------------------------\n')
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#50.24
+# ------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------
+# Compute the area of a circle, given the length of its radius.
+
+###################################################
+# Circle area formula
+# Student should enter statement on the next line.
+
+r = 8
+pi = 3.14
+
+result_6 = pi * r ** 2 
+
+print('Exercises 6:')
+print('Result exercise 6: {}'.format(result_6))
+print('\n----------------------------------------------\n')
+
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#200.96
+
+# ------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------
+# Compute the future value of a given present value, annual rate, and number of years.
+
+###################################################
+# Future value formula
+# Student should enter statement on the next line.
+
+p = 1000
+r = 7
+y = 10
+
+result_7 = 1000 * (  (1 + (0.01 * r)) ** y)
+
+print('Exercises 7:')
+print('Result exercise 7: {}'.format(result_7))
+print('\n----------------------------------------------\n')
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#1967.15135729
+# ------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------
+# Compute a name tag, given the first and last name.
+
+###################################################
+# Name tag formula
+# Student should enter statement on the next line.
+
+str_1 = "My name is"
+str_2 = "Joe"
+str_3 = "Warren"
+
+result_8 = str_1 + ' ' + str_2 + ' ' + str_3 + '.'
+
+print('Exercises 8:')
+print('Result exercise 8: {}'.format(result_8))
+print('\n----------------------------------------------\n')
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#My name is Joe Warren.
+# ------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------
+# Compute the statement about a person's name and age, given the person's name and age.
+
+###################################################
+# Name and age formula
+# Student should enter statement on the next line.
+
+str_1 = 'Joe Warren'
+
+result_9 =  str_1 + ' is ' +  str(52) + ' years old.'
+
+print('Exercises 9:')
+print('Result exercise 9: {}'.format(result_9))
+print('\n----------------------------------------------\n')
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#Joe Warren is 52 years old.
+
+# ------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------
+# Compute the distance between the points (x0, y0) and (x1, y1).
+
+###################################################
+# Distance formula
+# Student should enter statement on the next line.
+
+# Hint: You need to use the power operation ** .
+
+x_0 = 2
+x_1 = 2
+
+y_0 = 5
+y_1 = 6
+
+result_10 = ( (( x_0 - y_0) ** 2) + (( x_1 - y_1) ** 2) ) ** 0.5
+
+print('Exercises 10:')
+print('Result exercise 10: {}'.format(result_10))
+print('\n----------------------------------------------\n')
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#5.0
+
+# ------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------
